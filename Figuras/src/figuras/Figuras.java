74474ba95ca28537820e9902ca6079c21da31e43
dsvargasn <49066384+dsvargasn@users.noreply.github.com>
2019-04-08 11:57:00 -0500

    Add files via upload

---
+++ Figuras/src/figuras/Figuras.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+import figuras.logica.*;
+import java.util.Scanner;
+/**
+ *
+ * @author Estudiantes
+ */
+public class Figuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Seleccione la figura.");
+        System.out.println("1. Cuadrado, 2. Rectangulo.");
+        Scanner entrada = new Scanner (System.in);
+        int num = entrada.nextInt();
+        Figura f;
+        Punto origen = new Punto();
+        Punto fin = new Punto();
+        switch (num) {
+            case 1:
+                f = new Cuadrado();
+                origen.setX(0);
+                origen.setY(0);
+                
+                fin.setX(5);
+                fin.setY(0);
+                Mostrar(f, origen, fin);
+            break;
+            case 2:
+                f = new Rectangulo();
+                origen.setX(0);
+                origen.setY(5);
+                
+                fin.setX(10);
+                fin.setY(0);
+                Mostrar(f, origen, fin);
+            break;
+            default:
+                System.out.println("Opcioón no válida.");
+        }
+        
+    }
+    
+    public static void Mostrar(Figura f, Punto origen, Punto fin) {
+        
+        f.setOrigen(origen);
+        f.setFin(fin);
+        f.calcularArea();
+        f.calcularPerimetro();
+        System.out.println("Area = " + f.getArea());
+        System.out.println("Perímetro = " + f.getPerimetro());
+    }
+    
+}
---
+++ Figuras/src/figuras/logica/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras.logica;
+
+import figuras.logica.Prueba;
+/**
+ *
+ * @author Estudiantes
+ */
+public class Prueba {
+    
+    public void Prueba () {
+        Cuadrado c = new Cuadrado();
+        
+    }
+    
+}
